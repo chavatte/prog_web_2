5b4bdae585750918c4f952891cc3905aaf538d00
João Carlos Chavatte <chavatte@hotmail.com>
2022-07-27 19:26:16 -0300

    first commit

---
+++ src/routes/productRouter.js
@@ -0,0 +1,74 @@
+import { Router } from "express";
+import ProductDTO from "../views/productDTO.js";
+import {
+  atualizarProduto,
+  buscarUmProduto,
+  buscarTodosProdutos,
+  removerProduto,
+  salvarNovoPoduto,
+} from "../models/product.js";
+
+const productRouter = new Router();
+
+// CRUD - Create, Read, Update, Delete
+
+// Read
+productRouter.get("/:id?", async (req, res) => {
+  const { id } = req.params;
+
+  if (id) {
+    const produto = await buscarUmProduto(id);
+    if (!produto) {
+      res.status(200).send();
+      return;
+    }
+
+    const dto = new ProductDTO(produto);
+
+    res.status(200).json(dto.toJson());
+
+    return;
+  }
+
+  const listaDeDTOs = await buscarTodosProdutos().map((produto) => {
+    const dto = new ProductDTO(produto);
+    return dto.toJson();
+  });
+
+  res.status(200).json(listaDeDTOs);
+});
+
+// Create
+productRouter.post("/", async (req, res) => {
+  const { name, price } = req.body;
+
+  if (!name || !price) {
+    res.send(400).send("Falta parâmetros");
+    return;
+  }
+
+  const newProduct = salvarNovoPoduto(name, price);
+
+  res.status(201).json(newProduct);
+});
+
+// Update
+productRouter.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, price } = req.body;
+
+  const produto = await atualizarProduto(id, name, price);
+
+  res.status(200).json(produto);
+});
+
+// Delete
+productRouter.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  await removerProduto(id);
+
+  res.status(200).send("ok");
+});
+
+export default productRouter;
---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { cartRouter, productRouter, userRouter } from "./routes/index.js";
+import authorizationMiddleware from "./middlewares/authorization.js";
+
+const PORT = 3000;
+
+const server = express();
+
+server.use(express.json());
+
+server.use("/", userRouter);
+
+server.use(authorizationMiddleware);
+server.use("/product", productRouter);
+server.use("/cart", cartRouter);
+
+server.listen(PORT, () => {
+  console.log(`oi, estou funcionando na port ${PORT}`);
+});
