5458b314db5c47336e887159c94dfabe5f74d2df
José Otavio Carvalho <jose.carvalho@paipe.co>
2022-07-27 21:02:39 -0300

    fix small errors

--- src/models/token.js
+++ src/models/token.js
@@ -2,34 +2,34 @@ import mongoose from "mongoose";
 import moment from "moment";
 
 const TokenModel = mongoose.model("Token", {
-  createdAt: Date,
-  active: boolean,
-  userId: String,
+	createdAt: Date,
+	active: Boolean,
+	userId: String,
 });
 
 export const createToken = async (userId) => {
-  const token = new TokenModel();
+	const token = new TokenModel();
 
-  token.createdAt = new Date();
-  token.active = true;
-  token.userId = userId;
+	token.createdAt = new Date();
+	token.active = true;
+	token.userId = userId;
 
-  await token.save();
+	await token.save();
 
-  return token;
+	return token;
 };
 
 export const getValidToken = async (tokenId) => {
-  const token = await TokenModel.findById(tokenId);
+	const token = await TokenModel.findById(tokenId);
 
-  if (!token) throw new Error("Token inválido");
+	if (!token) throw new Error("Token inválido");
 
-  if (!token.active) throw new Error("Token inativo");
+	if (!token.active) throw new Error("Token inativo");
 
-  const expireAt = moment(token.createdAt);
-  expireAt.add(3, "d");
+	const expireAt = moment(token.createdAt);
+	expireAt.add(3, "d");
 
-  if (expireAt < moment()) throw new Error("Token inativo");
+	if (expireAt < moment()) throw new Error("Token inativo");
 
-  return token;
+	return token;
 };
